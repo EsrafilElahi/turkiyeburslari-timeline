bcf63d7f3ab76128a43697079114bd45d6d6f742
esrafil elahi <esrafil.elahi@gmail.com>
2018-01-08 14:54:16 +0330

    linting index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/index.css";
+import App from "./App";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
