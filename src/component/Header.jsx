3a6c88378fba1dc816a1d2721b29b15822dd062b
Esrafil <essinho1121@gmail.com>
2024-04-27 11:59:57 +0330

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='header'>
       <h1 className='header1'>react timeline</h1>
       <h1 className='header2'>for turkiye burslari 2021 application calendar</h1>
-    </div>
+    </div> 
   )
 }
 
