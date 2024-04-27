8deb49bbf67d0bc600a1f8ed63a09f4ac1332e08
Esrafil <essinho1121@gmail.com>
2024-04-27 12:00:05 +0330

    Update TLItem.jsx

--- src/component/TLItem.jsx
+++ src/component/TLItem.jsx
@@ -6,7 +6,7 @@ const TLItem = ({ info }) => {
 
   return (
     <div className='timeline-item'>
-      <div className='timeline-content'>
+      <div className='timeline-content'> 
         <span className='date'>{info.date}</span>
         <p className='title'>{info.title}</p>
         <p className='desc'>{info.desc}</p>
